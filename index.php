eb429e98a9dd81290751aa88f4b5f8806605abc5
Mikhail <madenne39@gmail.com>
2020-05-16 13:58:10 +0200

    learn v.1

--- index.php
+++ index.php
@@ -4,38 +4,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <title>PHP</title>
+    <title>Список дел</title>
 </head>
 
 <body>
-    <?php require 'header.php' ?>
-    <div class="container mt-5">
-        <h3 class="mb-5"> Статьи </h3>
-        <div class="d-flex flex-wrap">
-            <?php
-    for($i = 0; $i < 5; $i++): ?>
-            <div class="card mb-4 shadow-sm">
-                <div class="card-header">
-                    <h4 class="my-0 font-weight-normal">Текст</h4>
-                </div>
-                <div class="card-body">
-                    <img src="<?php echo ($i + 1)?>.jpg" class="img-thumbnail">
-                    <ul class="list-unstyled mt-3 mb-4">
-                        <li>10 users included</li>
-                        <li>2 GB of storage</li>
-                        <li>Email support</li>
-                        <li>Help center access</li>
-                    </ul>
-                    <button type="button" class="btn btn-lg btn-block btn-outline-primary">Подробнее</button>
-                </div>
-            </div>
-            <?php endfor; ?>
-        </div>
+    <div class="container">
+        <h1> Список дел</h1>
+        <form action="/add.php" method="post">
+        <input type="text" name="task" id="task" placeholder="Нужно сделать" class="form-control">
+        <button type="submit" name="sendTask" class="btn btn-success">Отправить</button>
+        </form> 
 
-    </div>
-   <?php require 'footer.php' ?>
+        <?php 
+        require 'configdb.php';
+        echo '<ul>';
+        $query = $pdo->query('SELECT * FROM `tasks` ORDER BY `id` DESC');
+        while ($row = $query->fetch(PDO::FETCH_OBJ)) {
+            echo '<li><b>'.$row->task.'</b><a href="/delete.php?id='.$row->id.'"><button> Удалить </button></li>';
+        }
+        echo '</ul>';
+        ?>
+
+    </div> 
 
 </body>
 
